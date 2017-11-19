8db73b61751241b7b76143df9aa3aa11bd8fa380
Peter Nuttall <psn@nuttall.im>
2017-11-19 05:58:26 +0000

    checkpoint

--- file-matcher.go
+++ file-matcher.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"time"
 )
 
 type stats struct {
@@ -15,21 +16,26 @@ type stats struct {
 }
 
 func processDir(dir string, stat *stats) ([]os.FileInfo, error) {
-	out, err := ioutil.ReadDir(dir)
+	fi, err := ioutil.ReadDir(dir)
 	stat.readdirs++
 	if err != nil {
 		stat.errors++
-		return out, err
+		return fi, err
 	}
-	for _, e := range out {
+	out := make([]os.FileInfo, 0, len(fi))
+	for _, e := range fi {
 		stat.files++
 		if e.IsDir() {
-			x, err := processDir(path.Join(dir, e.Name()))
+			p := path.Join(dir, e.Name())
+			x, err := processDir(p, stat)
 			out = append(out, x...)
 			if err != nil {
 				log.Print(err)
 			}
+		} else if e.Mode().IsRegular() && e.Size() > 0 {
+			out = append(out, e)
 		}
+
 	}
 	return out, nil
 }
@@ -37,6 +43,7 @@ func processDir(dir string, stat *stats) ([]os.FileInfo, error) {
 func main() {
 	dir := os.Args[1]
 	st := stats{}
+	start := time.Now()
 	fi, err := processDir(dir, &st)
 	if err != nil {
 		log.Fatal(err)
@@ -44,5 +51,18 @@ func main() {
 	for _, e := range fi {
 		fmt.Println(e)
 	}
-	fmt.Println(st)
+	fmt.Println(time.Since(start))
+	fmt.Println("stats")
+	fmt.Printf("dirs %d\n", st.readdirs)
+	fmt.Printf("errors %d\n", st.errors)
+	fmt.Printf("files %d\n", st.files)
+
+	sizeToFiles := make(map[int64][]os.FileInfo)
+	for _, e := range fi {
+		sizeToFiles[e.Size()] = append(sizeToFiles[e.Size()], e)
+	}
+	for k, v := range sizeToFiles {
+		fmt.Println(k, v)
+	}
+
 }
