a847cf8c719ec38b03b0d010e665b426de36009e
Peter Nuttall <psn@nuttall.im>
2017-12-05 13:45:59 +0000

    checkpoint

--- file-matcher_test.go
+++ file-matcher_test.go
@@ -1,7 +1,27 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestProcessDir(t *testing.T) {
-	c
+	st := stats{}
+	tmpDir, err := ioutil.TempDir(os.Getenv("TMPDIR"), "file-matcher-test")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	// empty dir
+	files, err := processDir(tmpDir, &st)
+	if err != nil {
+		t.Errorf("unexpected error %v", err)
+	}
+	if len(files) != 0 {
+		t.Errorf("%v", files)
+	}
+	want := stats{}
+	if st != want {
+		t.Errorf("%v", st)
+	}
 }
